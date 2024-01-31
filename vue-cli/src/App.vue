c7ee17f84394047cc504bccfa709b5cba55279cd
miyeon <kmy930901@naver.com>
2024-01-31 21:28:09 +0900

    싱글 파일 컴포넌트 체계에서 컴포넌트 등록하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div> <!-- root element가 하나만 있어야 함 -->
     {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
 // new Vue ({
 //   data: {
 //     str: 'hi'
@@ -15,6 +22,9 @@ export default {
     return {
       str: 'hi'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 </script>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<!-- 컴포넌트의 이름은 최소한 2단어로 한다.
+html 표준 태그인지 컴포넌트 태그인지 브라우저가 알 길이 없음. -->
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
